f15de7bd24aa4acdf1443ee32240546b5f947c14
miku <michael.jaser@peerigon.com>
2014-02-10 10:45:05 +0100

    - added test for prepare request

---
+++ test/unit/lib/transport/prepareRequest.test.js
@@ -0,0 +1,59 @@
+"use strict";
+
+var chai = require("chai"),
+    expect = chai.expect,
+    http = require("http");
+
+chai.Assertion.includeStack = true;
+
+var prepareRequest = require("../../../../lib/transport/prepareRequest.js");
+
+describe("prepareRequest()", function () {
+    var req;
+
+    beforeEach(function () {
+        req = new http.IncomingMessage({});
+    });
+
+    describe("#ALL", function () {
+
+        it("should parse the querys string and attach it to req", function () {
+
+            req.method = "POST";
+            req.url = "http://localhost:3000/api?band=moderat";
+
+            prepareRequest(req);
+
+            expect(req.query).to.eql({ band: "moderat" });
+        });
+    });
+
+    describe("#GET", function () {
+
+        it("should set query as body if no body was passed", function () {
+
+            req.method = "GET";
+            req.url = "http://localhost:3000/api?band=moderat";
+            req.body = {};
+
+            prepareRequest(req);
+
+            expect(req.query).to.eql({ band: "moderat" });
+            expect(req.body).to.eql({ band: "moderat" });
+        });
+
+        it("should not set query as body if a body was passed", function () {
+
+            req.method = "GET";
+            req.url = "http://localhost:3000/api?band=moderat";
+            req.body = {
+                song: "a new error"
+            };
+
+            prepareRequest(req);
+
+            expect(req.query).to.eql({ band: "moderat" });
+            expect(req.body).to.eql({ song: "a new error" });
+        });
+    });
+});
