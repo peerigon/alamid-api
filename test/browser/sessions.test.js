73e2657219bee1a4d7f727e6ec5931d8f1d9faca
meaku <michael.jaser@peerigon.com>
2014-06-12 18:53:48 +0200

    - added testling browser test for session support

---
+++ test/browser/sessions.test.js
@@ -0,0 +1,191 @@
+"use strict";
+
+var chai = require("chai"),
+    httpRequest = require("browser-request"),
+    socketIo = require("../../node_modules/socket.io/node_modules/socket.io-client/index.js"),
+    expect = chai.expect,
+    when = require("when");
+
+chai.config.includeStack = true;
+
+var socket,
+    host = "http://" + location.host;
+
+function request(options) {
+
+    request.type = request.type || "http";
+
+    if (options.url.indexOf("http") === -1) {
+        options.url = host + options.url;
+    }
+
+    console.log("Request > " + options.url);
+
+    return when.promise(function (resolve, reject) {
+
+        if (request.type === "http") {
+
+            if (typeof(options.body) === "object") {
+                options.headers = {"content-type": "application/json"};
+                options.body = JSON.stringify(options.body);
+                options.jar = true; //remember cookies
+                options.withCredentials = true;
+            }
+
+            httpRequest(options, function (err, response, body) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                if (typeof body !== "object") {
+                    body = JSON.parse(body);
+                }
+
+                resolve(body);
+            });
+        }
+        else if (request.type === "ws") {
+            socket.emit("request", options, function (res) {
+                if (typeof res !== "object") {
+                    res = JSON.parse(res);
+                }
+                resolve(res);
+            });
+        }
+        else {
+            reject(new Error("Invalid transport '" + request.type + "'"));
+        }
+    });
+}
+
+describe("session support", function () {
+
+    //initialize session
+    before(function (done) {
+        request.type = "http";
+
+        request({
+            url: "/test",
+            method: "get"
+        }).done(function () {
+                done();
+            },
+            done);
+    });
+
+
+    before(function (done) {
+       socket = socketIo().on("connect", done);
+    });
+
+    describe("#http", function () {
+
+        it("should be able to save and read value from/to the session", function (done) {
+            request({
+                url: "/test/?test=http1",
+                method: "get"
+            })
+                .then(function() {
+                    return request({
+                        url: "/test",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("http1");
+
+                    return request({
+                        url: "/test/?test=http2",
+                        method: "get"
+                    });
+                })
+                .then(function() {
+                    return request({
+                        url: "/test/",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("http2");
+                    done();
+                })
+                .catch(done);
+        });
+
+    });
+
+    describe("#ws", function () {
+
+        it("should be able to save and read value from/to the session", function (done) {
+            request({
+                url: "/test/?test=ws1",
+                method: "get"
+            })
+                .then(function() {
+                    return request({
+                        url: "/test",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("ws1");
+
+                    return request({
+                        url: "/test/?test=ws2",
+                        method: "get"
+                    });
+                })
+                .then(function() {
+                    return request({
+                        url: "/test/",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("ws2");
+                    done();
+                })
+                .catch(done);
+        });
+    });
+
+    describe("#mixed ws/http", function () {
+
+        it("should be able to save and read value from/to the session via http and ws", function (done) {
+            request({
+                url: "/test/?test=viaHttp",
+                method: "get",
+                type: "http"
+            })
+                .then(function() {
+                    return request({
+                        type: "ws",
+                        url: "/test",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("viaHttp");
+
+                    return request({
+                        type: "ws",
+                        url: "/test/?test=viaWs",
+                        method: "get"
+                    });
+                })
+                .then(function() {
+                    return request({
+                        type: "http",
+                        url: "/test/",
+                        method: "get"
+                    });
+                })
+                .then(function (body) {
+                    expect(body.data.session).to.eql("viaWs");
+                    done();
+                })
+                .catch(done);
+        });
+    });
+});
